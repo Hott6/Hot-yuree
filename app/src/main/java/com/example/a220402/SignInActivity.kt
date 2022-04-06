e38bbdb8f7636b5309b99dd921f3a99ae1add219
yuree <yuree0217@gmail.com>
2022-04-07 01:58:42 +0900

    [feat] 필수과제

--- app/src/main/java/com/example/a220402/SignInActivity.kt
+++ app/src/main/java/com/example/a220402/SignInActivity.kt
@@ -7,19 +7,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.a220402.databinding.ActivitySignInBinding
+import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignInActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val intent = Intent(this, HomeActivity::class.java)
+        val intent2 = Intent(this, SignUpActivity::class.java)
+
         binding.btn.setOnClickListener {
-            Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
+            if (binding.etId.text.isNullOrBlank() || binding.etPw.text.isNullOrBlank()) {
+                Toast.makeText(this, "아이디/비밀번호를 확인해주세요", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                startActivity(intent)
+            }
+        }
+        binding.btnJ.setOnClickListener {
+            startActivity(intent2)
         }
     }
 }
--- app/src/main/java/com/example/a220402/SignUpActivity.kt
+++ app/src/main/java/com/example/a220402/SignUpActivity.kt
@@ -1,11 +1,25 @@
 package com.example.a220402
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.a220402.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnF.setOnClickListener {
+            if (binding.etName.text.isNullOrBlank() || binding.etId.text.isNullOrBlank() || binding.etPw.text.isNullOrBlank()) {
+                Toast.makeText(this, "입력되지 않은 정보가 있습니다", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                finish()
+            }
+        }
     }
 }
