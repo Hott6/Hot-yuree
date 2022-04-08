4f00fc4f3c2a2ffb13fe860c203c864281238975
yuree <yuree0217@gmail.com>
2022-04-09 01:08:18 +0900

    [feat] 성장과제

--- app/src/main/java/com/example/a220402/SignUpActivity.kt
+++ app/src/main/java/com/example/a220402/SignUpActivity.kt
@@ -1,9 +1,12 @@
 package com.example.a220402
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.a220402.databinding.ActivitySignUpBinding
+import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
@@ -13,11 +16,16 @@ class SignUpActivity : AppCompatActivity() {
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnF.setOnClickListener {
+
+        binding.btnFinshSignup.setOnClickListener {
             if (binding.etName.text.isNullOrBlank() || binding.etId.text.isNullOrBlank() || binding.etPw.text.isNullOrBlank()) {
                 Toast.makeText(this, "입력되지 않은 정보가 있습니다", Toast.LENGTH_SHORT).show()
-            }
-            else {
+            } else {
+                val intent = Intent(this@SignUpActivity, SignInActivity::class.java) //signinactivity에 대한 intent 객체 생성
+                intent.putExtra("id", et_id.text.toString()) //id에 et_id 데이터 담음
+                intent.putExtra("pw", et_pw.text.toString()) //마찬가지로 pw에 et_pw 담음
+                setResult(Activity.RESULT_OK, intent) //result_ok인 경우 SignInActivitiy로 intent 객체 보냄
+
                 finish()
             }
         }
