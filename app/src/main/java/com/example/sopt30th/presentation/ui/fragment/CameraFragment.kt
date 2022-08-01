52224dcbef0caa1efbb7a5dbaa44bbad89ab60d9
uxri <yuree0217@g.eulji.ac.kr>
2022-08-01 21:53:46 +0900

    [FEATURE] BaseFragment CameraFragment에 적용

--- app/src/main/java/com/example/sopt30th/presentation/ui/fragment/CameraFragment.kt
+++ app/src/main/java/com/example/sopt30th/presentation/ui/fragment/CameraFragment.kt
@@ -7,29 +7,26 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.provider.MediaStore
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
-import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
+import com.example.sopt30th.R
 import com.example.sopt30th.databinding.FragmentCameraBinding
+import com.example.sopt30th.presentation.ui.base.BaseFragment
 
-class CameraFragment : Fragment() {
-    private var _binding: FragmentCameraBinding? = null
-    private val binding get() = _binding!!
+class CameraFragment : BaseFragment<FragmentCameraBinding>() {
+    override val TAG: String
+        get() = CameraFragment::class.java.simpleName
+    override val layoutRes: Int
+        get() = R.layout.fragment_camera
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentCameraBinding.inflate(inflater, container, false)
-        requestPermissionLauncher
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         changeProfileImage()
-        return binding.root
     }
 
     private val requestPermissionLauncher =
@@ -77,9 +74,5 @@ class CameraFragment : Fragment() {
                 }
             }
         }
-    }
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
